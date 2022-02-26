1146b1be931881bb271d9625c4c3429f12116b75
Ajprojectss <info.educationajay17@gmail.com>
2022-02-26 15:00:34 +0530

    adding a new file to git

---
+++ src/php/Php.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name: Ajay Yadav
+ * IDE: IntelliJ IDEA Community Edition
+ * Date: 26-02-2022
+ */
+
+package php;
+
+public class Php {
+    public static void main(String[] args) {
+        System.out.println("Learn to adding the new file to github");
+    }
+}
